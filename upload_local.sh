404b5c7ccb183f3254f52c97e3bd4a141accd936
Conrad S <conrad.schloer@gmail.com>
2022-12-20 15:50:05 -0500

    Reduce size of initial image

--- upload_local.sh
+++ upload_local.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 AWS_ACCESS_KEY_ID=minioadmin AWS_SECRET_ACCESS_KEY=minioadmin aws s3 cp ./src s3://wallcity/ --recursive --endpoint http://localhost:9000
+echo "http://localhost:9000/wallcity/index.html"
